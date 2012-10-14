69e063a802eab26c57e209e4c0f48f3c4e0a4061
Atsushi Tadokoro <tadokoro@gmail.com>
2012-10-14 13:27:59 +0900

    initial commit

---
+++ 6_Closing/src/SawSynth.h
@@ -0,0 +1,32 @@
+//
+//  SawSynth.h
+//  ofxSuperColliderTest
+//
+//  Created by Atsushi Tadokoro on 2012/10/03.
+//
+//
+
+
+#pragma once
+
+#include "ofMain.h"
+#include "ofxSuperCollider.h"
+
+class SawSynth {
+public:
+    SawSynth(int n);
+    ~SawSynth();
+    void update();
+    void draw();
+    void clear();
+    
+    ofxSCSynth *synth;
+    int nth;
+    float freq;
+    float detune;
+    int alpha;
+    float startTime;
+    float phase;
+    float height;
+    bool death;
+};
